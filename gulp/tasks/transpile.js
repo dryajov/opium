37f19d551ffbb3cb2b222dc52a552e6da586b6b9
dryajov <dryajov@gmail.com>
2015-08-15 14:36:00 -0600

    removing copy of node modules

--- gulp/tasks/transpile.js
+++ gulp/tasks/transpile.js
@@ -8,13 +8,10 @@ var babel = require("gulp-babel");
 var concat = require("gulp-concat");
 
 gulp.task("transpile", function () {
-    gulp.src(['node_modules/babel/**/*'])
-        .pipe(gulp.dest("opium-es5/node_modules"));
-
     return gulp.src("app/**/*.js")
         .pipe(sourcemaps.init())
         .pipe(babel())
         .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest("opium-es5"));
+        .pipe(gulp.dest("lib"));
 
 });
